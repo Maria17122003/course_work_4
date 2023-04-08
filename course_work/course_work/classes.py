50747e061c6dad462e96d014a068045c55b0cfee
Maria <mararyabina62@gmail.com>
2023-04-08 22:42:14 +1000

    Добавили новый класс

--- course_work/course_work/classes.py
+++ course_work/course_work/classes.py
@@ -1,29 +1,30 @@
 import json
 import os
 from abc import ABC, abstractmethod
-from pprint import pprint
-
 import requests
 
 
 class Engine(ABC):
+    """
+    Абстрактный класс
+    """
     @abstractmethod
     def get_request(self):
         pass
 
-    @staticmethod
-    def get_connector(file_name):
-        """
-        Возвращает экземпляр
-        класса Connector
-        """
-        pass
-
 
 class HH(Engine):
+    """
+    Класс платформы
+    HH
+    """
     URL = 'https://api.hh.ru/vacancies/'
 
     def __init__(self, search_keyword):
+        """
+        Инициализирует слово,
+        по которому происходит поиск вакансий
+        """
         super().__init__()
         self.search_keyword = search_keyword
         self.params = {
@@ -34,6 +35,9 @@ class HH(Engine):
             }
 
     def get_request(self):
+        """
+        Получает вакансии по API
+        """
         response = requests.get(self.URL, params=self.params)
         data = response.content.decode()
         response.close()
@@ -41,6 +45,10 @@ class HH(Engine):
         return json_hh
 
     def info_vacancies_hh(self, vacancies):
+        """
+        Структурирует получаемые
+        из API данные по ключам
+        """
         data = {
                 'from': 'HeadHunter',
                 'name': vacancies.get('name'),
@@ -58,8 +66,13 @@ class HH(Engine):
 
     @property
     def get_vacancies_hh(self):
+        """
+        Получает вакансии
+        при наличии информации
+        о ЗП
+        """
         vacancies_hh = []
-        while len(vacancies_hh) <= 500:
+        while len(vacancies_hh) <= 1000:
             data = self.get_request()
             items = data.get('items')
             if not items:
@@ -73,19 +86,40 @@ class HH(Engine):
         return vacancies_hh
 
     def to_json(self, data):
+        """
+        Создает файл,
+        добавляет в него
+        вакансии
+        """
         with open("vacancies_hh.json", "w", encoding="UTF-8") as name_file:
             json.dump(data, name_file, indent=2, ensure_ascii=False)
 
 
 class Superjob(Engine):
+    """
+    Класс платформы
+    Superjob
+    """
     URL = 'https://api.superjob.ru/2.0/vacancies/'
 
     def __init__(self, search_keyword):
+        """
+        Инициализирует слово,
+        по которому происходит поиск вакансий
+        """
         super().__init__()
         self.search_keyword = search_keyword
-        self.params = {'keywords': f'{search_keyword}', 'count': 100, 'page': 0}
+        self.params = {
+            'keywords': f'{search_keyword}',
+            'count': 100,
+            'area': 113,
+            'page': 0
+        }
 
     def get_request(self):
+        """
+        Получает вакансии по API
+        """
         api_key: str = os.getenv('api_key')
         headers = {
             'Host': 'api.superjob.ru',
@@ -97,10 +131,14 @@ class Superjob(Engine):
         data = response.content.decode()
         response.close()
 
-        js_superjob = json.loads(data)
-        return js_superjob
+        sj_superjob = json.loads(data)
+        return sj_superjob
 
     def info_vacancies_sj(self, vacancies):
+        """
+        Структурирует получаемые
+        из API данные по ключам
+        """
         data = {
                 'from': 'Superjob',
                 'name': vacancies.get('profession'),
@@ -116,14 +154,19 @@ class Superjob(Engine):
 
     @property
     def get_vacancies_sj(self):
+        """
+        Получает вакансии
+        при наличии информации
+        о ЗП
+        """
         vacancies_sj = []
-        while len(vacancies_sj) <= 500:
+        while len(vacancies_sj) <= 1000:
             data = self.get_request()
             items = data.get('objects')
             if not items:
                 break
             for vacancy_sj in items:
-                if vacancy_sj.get('payment_from') is not None and vacancy_sj.get('currency') == 'rub':
+                if vacancy_sj['payment_from'] is not None and vacancy_sj.get('currency') == 'rub':
                     vacancies_sj.append(self.info_vacancies_sj(vacancy_sj))
 
             self.params['page'] += 1
@@ -131,12 +174,25 @@ class Superjob(Engine):
         return vacancies_sj
 
     def to_json(self, data):
+        """
+        Создает файл,
+        добавляет в него
+        вакансии
+        """
         with open("vacancies_superjob.json", "w", encoding="UTF-8") as name_file:
             json.dump(data, name_file, indent=2, ensure_ascii=False)
 
 
 class Vacancy:
+    """
+    Класс для вакансий
+    """
     def __init__(self, name_vacancy, url_vacancy, description, salary):
+        """
+        Инициализирует
+        название вакансии, ссылку,
+        описание и зарплату
+        """
         self.name_vacancy = name_vacancy
         self.url_vacancy = url_vacancy
         self.description = description
@@ -147,8 +203,7 @@ class Vacancy:
 
     def __lt__(self, other):
         """
-        Сравнивает количество
-        подписчиков
+        Сравнивает зарплату
         """
         if int(self.salary) < int(other.salary):
             return True
@@ -156,15 +211,82 @@ class Vacancy:
 
     def __gt__(self, other):
         """
-        Сравнивает количество
-        подписчиков
+        Сравнивает зарплату
         """
         if int(self.salary) > int(other.salary):
             return True
         return False
 
+    def get_list_vacancies(self):
+        """
+        Структурирует данные
+        о вакансиях
+        по ключам
+        """
+        data = {
+            'name': self.name_vacancy,
+            'url': self.url_vacancy,
+            'description': self.description,
+            'salary': self.salary,
+        }
+
+        return data
+
 
 class JSONSaver:
-    def add_vacancy(self):
-        like_vacancy = []
+    """
+    Класс для сохранения
+    понравившихся вакансий
+    """
+    def __init__(self):
+        self.vacancies = []
 
+    def like_vacancy(self, platform, url):
+        """
+        Добавляет в список
+        понравившиеся вакансии
+        """
+        cache = []
+        if platform == "HeadHunter":
+            with open('vacancies_hh.json', 'r', encoding='utf-8') as name_file:
+                data = json.load(name_file)
+            for item in data:
+                if url == item['url']:
+                    cache.append(item)
+                    for i in cache:
+                        if i not in self.vacancies:
+                            self.vacancies.append(item)
+
+        else:
+            with open('vacancies_superjob.json', 'r', encoding='utf-8') as name_file:
+                data = json.load(name_file)
+            for item in data:
+                if url == item['url']:
+                    cache.append(item)
+                    for i in cache:
+                        if i not in self.vacancies:
+                            self.vacancies.append(item)
+
+        return self.vacancies
+
+    def delete_vacancy(self, del_url_vacancy):
+        """
+        Удаляет из списка
+        вакансии
+        """
+        count = 0
+        for txt in self.vacancies:
+            if txt['url'] == del_url_vacancy:
+                self.vacancies.pop(count)
+            else:
+                count += 1
+
+        return self.vacancies
+
+    def list_vacancy(self):
+        """
+        Сохраняет список
+        в файл
+        """
+        with open("vacancies_like.json", "w", encoding="UTF-8") as name_file:
+            json.dump(self.vacancies, name_file, indent=2, ensure_ascii=False)
